e46b6554394b63d3240eba3824e6f06d970e9082
Grace Manning <gracebmanning@gmail.com>
2026-02-27 19:04:52 -0800

    adjust spacing on standard layout

--- components/standardLayout.tsx
+++ components/standardLayout.tsx
@@ -4,7 +4,9 @@ export default function StandardLayout({ content }: { content: React.ReactNode }
     return (
         <div className="w-full h-screen flex flex-row justify-start items-start overflow-hidden">
             <NavMenu isHome={false} />
-            <main className="h-full w-full ml-53.75 p-4 overflow-y-auto">{content}</main>
+            <main className="h-full w-full ml-53.75 px-4 pt-6 pb-20 overflow-y-auto">
+                {content}
+            </main>
         </div>
     );
 }
